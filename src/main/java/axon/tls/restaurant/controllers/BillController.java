dfb1972a8ff1a9078d4d3d19428d768896dc24c3
tlsmimi1111 <tranhoangson16997@gmail.com>
2019-12-03 21:11:55 +0700

    Update BillController.java
    add another annotation

--- src/main/java/axon/tls/restaurant/controllers/BillController.java
+++ src/main/java/axon/tls/restaurant/controllers/BillController.java
@@ -1,8 +1,10 @@
 package axon.tls.restaurant.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class BillController {
 
 }
